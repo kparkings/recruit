4d8671da96f89af3f3da486ea24de5c271729da1
kevin parkings <kparkings@gmail.com>
2021-05-01 23:04:27 +0200

    gui best guess language options from country selection

--- recruit-gui/src/app/new-candidate/new-candidate.component.ts
+++ recruit-gui/src/app/new-candidate/new-candidate.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit }                              	from '@angular/core';
-import { ReactiveFormsModule, FormGroup, FormControl }    	from '@angular/forms';
-import { CandidateServiceService }                        	from '../candidate-service.service';
-import { CurriculumService }                        		from '../curriculum.service';
-import { CandidateFunction }                              	from '../candidate-function';
-import {NgbModal, NgbModalOptions, ModalDismissReasons}   	from '@ng-bootstrap/ng-bootstrap';
-import {TemplateRef, ViewChild,ElementRef, AfterViewInit  } from '@angular/core';
-import { Router} 											from '@angular/router';
+import { Component, OnInit }                                                                   from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl }                   from '@angular/forms';
+import { CandidateServiceService }                                                          from '../candidate-service.service';
+import { CurriculumService }                                                                     from '../curriculum.service';
+import { CandidateFunction }                                                                    from '../candidate-function';
+import {NgbModal, NgbModalOptions, ModalDismissReasons}            from '@ng-bootstrap/ng-bootstrap';
+import {TemplateRef, ViewChild,ElementRef, AfterViewInit  }                 from '@angular/core';
+import { Router}                                                                                         from '@angular/router';
 
 @Component({
   selector: 'app-new-candidate',
@@ -20,21 +20,21 @@ export class NewCandidateComponent implements OnInit {
    
    public formBean:FormGroup = new FormGroup({
      
-    candidateId:      new FormControl(''),
-    firstname:        new FormControl(),
-    surname:          new FormControl(),
-    email:            new FormControl(),
-    roleSought:       new FormControl(),
-    country:          new FormControl(),
-    city:             new FormControl(),
-    perm:             new FormControl(),
-    freelance:        new FormControl(),
-    dutch:            new FormControl(),
-    english:          new FormControl(),
-    french:           new FormControl(),
-    function:         new FormControl(),
-    yearsExperience:  new FormControl(),
-    skills:           new FormControl()
+       candidateId:                    new FormControl(''),
+       firstname:                        new FormControl(),
+       surname:                         new FormControl(),
+       email:                              new FormControl(),
+       roleSought:                     new FormControl(),
+       country:                           new FormControl(),
+       city:                                 new FormControl(),
+       perm:                              new FormControl(),
+       freelance:                       new FormControl(),
+       dutch:                             new FormControl(),
+       english:                           new FormControl(),
+       french:                            new FormControl(),
+       function:                         new FormControl(),
+       yearsExperience:            new FormControl(),
+       skills:                               new FormControl()
 
   });
 
@@ -54,30 +54,21 @@ export class NewCandidateComponent implements OnInit {
   */
   ngOnInit(): void {
 
-  //satrt
-  //  this.candidateService.authenticate().subscribe( data => {
-   //   console.log("Done");
-    //});
-    //end
-
   }
 
   /**
   * Registers a new Candidate with the backend
   */
   public addCandidate(): void {
-    //TODO: Convert Formbean to object before passing to Service
     this.candidateService.addCandidate(this.formBean).subscribe(d=>{
       this.open('feedbackBox', "Success",  true)
     });
   };
 
-  public feedbackBoxClass:string = '';
-  public feedbackBoxTitle = '';
-  public feedbackBoxText:string = '';
+  public feedbackBoxClass:string            = '';
+  public feedbackBoxTitle                       = '';
+  public feedbackBoxText:string             = '';
 
-
-//this.open('feedbackBox', "Success",  true);
 
   public open(content:any, msg:string, success:boolean):void {
     
@@ -123,5 +114,53 @@ export class NewCandidateComponent implements OnInit {
     	});
   		
   }
-
+  
+  public selectOptionLangDutch:string = '';
+  public selectOptionLangEnglish:string = '';
+  public selectOptionLangFrench:string = '';
+ 
+  public isSelected(language:string):void{
+      console.log("XXX1");
+      switch(language) {
+          case 'dutch':{
+           //   this.selectOptionLangDutch = this.formBean.get('dutch')!.value;
+             return;
+          } 
+          case 'english':{
+              return;
+          } 
+      }
+      
+  }
+  
+  /**
+  * Updates Language options with best guesses based upon the country 
+  * selected for the Candidate 
+  */
+  public updateCountry():void {
+      
+      let country:string = this.formBean.get('country') === null ? "" : this.formBean.get('country')!.value;
+      
+      switch(country) {
+          case 'UK':{
+              this.formBean.get('dutch')!.setValue('NO');
+              this.formBean.get('english')!.setValue('YES');
+              this.formBean.get('french')!.setValue('NO');
+              break;
+          } 
+          case 'NETHERLANDS':{
+              this.formBean.get('dutch')!.setValue('NO');
+              this.formBean.get('english')!.setValue('YES');
+              this.formBean.get('french')!.setValue('NO');
+              break;
+          } 
+          case 'BELGIUM':{
+              this.formBean.get('dutch')!.setValue('NO');
+              this.formBean.get('english')!.setValue('NO');
+              this.formBean.get('french')!.setValue('NO');
+              break;
+          } 
+      }
+      
+  }
 }
