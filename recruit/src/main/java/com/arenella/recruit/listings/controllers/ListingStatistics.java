8e6abb8707b59865de0fa00e7df9b5d4e1a78520
kevin parkings <kparkings@gmail.com>
2022-03-03 18:02:30 +0100

    bug fix leading 0s on years

--- recruit/src/main/java/com/arenella/recruit/listings/controllers/ListingStatistics.java
+++ recruit/src/main/java/com/arenella/recruit/listings/controllers/ListingStatistics.java
@@ -65,7 +65,7 @@ public class ListingStatistics {
 		
 		TemporalField 	weekOfYear = WeekFields.of(Locale.getDefault()).weekOfYear();
 		int 			weekNumber = LocalDate.now().get(weekOfYear);
-		String 			key = LocalDate.now().getYear() + " - " + weekNumber;
+		String 			key = LocalDate.now().getYear() + " - " + String.format("%02d", weekNumber);;
 		
 		if (this.viewsPerWeek.containsKey(key)) {
 			this.viewsThisWeek = this.viewsPerWeek.get(key);
@@ -82,7 +82,7 @@ public class ListingStatistics {
 		TemporalField 	weekOfYear = WeekFields.of(Locale.getDefault()).weekOfYear();
 		int 			weekNumber = event.getCreated().get(weekOfYear);
 		
-		String 			key = event.getCreated().getYear() + " - " + weekNumber;
+		String 			key = event.getCreated().getYear() + " - " + String.format("%02d", weekNumber);;
 		
 		if (viewsPerWeek.containsKey(key)) {
 			viewsPerWeek.put(key, viewsPerWeek.get(key) + 1 );
