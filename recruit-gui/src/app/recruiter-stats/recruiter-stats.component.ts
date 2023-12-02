4b2e2e732532d531ead16b03b798f2a3540abfaf
kevin parkings <kparkings@gmail.com>
2023-12-02 18:13:41 +0100

    Reruiter stats charts working

--- recruit-gui/src/app/recruiter-stats/recruiter-stats.component.ts
+++ recruit-gui/src/app/recruiter-stats/recruiter-stats.component.ts
@@ -1,17 +1,17 @@
-import { Component } 									from '@angular/core';
+import { Component, OnInit } 									from '@angular/core';
 import { RecruiterListingStatistics, ListingStat } 		from '../recruiter-listing-statistics';
 import { StatisticsService } 							from '../statistics.service';
 import { ChartData}										from './chart-data';
 import { NgChartsModule } 								from 'ng2-charts';
 import { Router}										from '@angular/router';
-import { ChartType } from 'chart.js';
+import { Chart, ChartType } from 'chart.js';
 
 @Component({
   selector: 'app-recruiter-stats',
   templateUrl: './recruiter-stats.component.html',
   styleUrls: ['./recruiter-stats.component.css']
 })
-export class RecruiterStatsComponent {
+export class RecruiterStatsComponent implements OnInit{
 	
 	private recruiterId:string 									= "";
 	public  listingStatistics:RecruiterListingStatistics 		= new RecruiterListingStatistics();
@@ -25,24 +25,29 @@ export class RecruiterStatsComponent {
 	public 	listingViewsThisWeek:number 						= 0;
 	public  jobBoardType:ChartType = 'line';
 
+	listingViewsWeekKeys:string[] 			= new Array<string>();
+	listingViewWeekValues:string[] 			= new Array<string>();
+	
+
+	/** Charts */
+	public chartColor:string = 'grey';
+	
+	
 	/**
 	* Constructor
 	* @param statisticsService - Services relating to statistics
 	*/
 	public constructor(public statisticsService:StatisticsService, private router:Router){
-		this.refreshData();
+		//this.refreshData();
 	}
 
-	/**
-	* Fetches data from backend 
-	*/	
-	public refreshData():void{
+	ngOnInit(): void {
+		this.refreshDataGeneralStats();
+		this.refreshDataRecruiterListingStats();
 		
-		this.recruiterId = sessionStorage.getItem("userId") ? sessionStorage.getItem("userId")+"" : "";
-		this.statisticsService.fetchRecruiterListingStats(this.recruiterId).subscribe(stats =>  {
-			this.listingStatistics = Object.assign(new RecruiterListingStatistics(), stats);
-			this.switchListing(this.selectedListing);
-		});	
+	}
+
+	refreshDataGeneralStats():void{
 		
 		this.statisticsService.getTotalNumberOfActiceCandidatesStatistics().forEach(count => {
 			this.totalNumberActiveCandidates = count;
@@ -52,7 +57,7 @@ export class RecruiterStatsComponent {
 		
 			let stats:any[] = data;
 			
-			let functionStatCount:Array<number> 		= new Array<number>(); 
+			let functionStatCount:Array<string> 		= new Array<string>(); 
 			let functionStatName:Array<string> 			= new Array<string>(); 
 				
 			stats.forEach(functonStat => {
@@ -62,24 +67,75 @@ export class RecruiterStatsComponent {
 				
 			});
 			
-			//this.availabilityChart.chartData	= [{ data: functionStatCount, label: 'Function' },];
-			//this.availabilityChart.chartLabels 	= functionStatName;
+			//START
+			this.availabilityChartData = [
+	           	{
+	            label: "Todays Logins",
+	       	    data: functionStatCount,
+	            backgroundColor: this.chartColor
+	          },
+	        	];
+			this.availabilityChartLabels = functionStatName; 
+			//this.switchTab(this.currentTab);
+			//END
 			
-    	}).catch(err => {this.handleSessionEnded(err)});;
+    	});
     	
     	this.statisticsService.getListingStats().subscribe(listingData => {
 					
 					this.listingViewsToday 		= listingData.viewsToday;
 					this.listingViewsThisWeek 	= listingData.viewsThisWeek;
-				
-					//KP WE COMMENTED THIS OUT WHEN WORKING ON STATISTICS. WOULD HAVE BEEN ENABLED !!!!!!!!!!!!!!!!!!!!!!!!!!
-					//let listingChartViews:number[] 		= Object.values(listingData.viewsPerWeek);
-					//let listingChartViewsKeys:string[] 	= Object.keys(listingData.viewsPerWeek);
-		
-			//		this.jobboardChart.chartData = [{ data: listingChartViews, label: 'Downloads' },];
-			//		this.jobboardChart.chartLabels = listingChartViewsKeys;
 					
-				}, err => { this.handleSessionEnded(err)});
+					this.listingViewsWeekKeys 		= listingData.viewsPerWeek.map(aa => aa.bucketName);
+					this.listingViewWeekValues 		= listingData.viewsPerWeek.map(aa => aa.count);
+					
+					console.log(JSON.stringify(this.listingViewWeekValues));
+					console.log(JSON.stringify(this.listingViewsWeekKeys));
+					//START
+					this.listingsChartData = [
+			           	{
+			            label: "Job Board Views",
+			       	    data: this.listingViewWeekValues,
+			            backgroundColor: this.chartColor
+			          },
+			        	];
+					this.listingsChartLabels = this.listingViewsWeekKeys; 
+					//END
+					this.switchTab(this.currentTab);
+					
+				}, err => {
+					
+					console.log("ERR " + JSON.stringify(err));
+			if (err.status === 401 || err.status === 0) {
+				sessionStorage.removeItem('isAdmin');
+				sessionStorage.removeItem('isRecruter');
+				sessionStorage.removeItem('isCandidate');
+				sessionStorage.removeItem('loggedIn');
+				sessionStorage.setItem('beforeAuthPage', 'view-candidates');
+				this.router.navigate(['login-user']);
+			}
+    	});
+    	
+	}
+	
+	refreshData():void{
+		if(this.currentTab == 'general-stats'){
+			this.refreshDataGeneralStats();
+		}else {
+			this.refreshDataRecruiterListingStats();
+		}
+	}
+	
+	
+	refreshDataRecruiterListingStats():void{
+		
+		this.recruiterId = sessionStorage.getItem("userId") ? sessionStorage.getItem("userId")+"" : "";
+		this.statisticsService.fetchRecruiterListingStats(this.recruiterId).subscribe(stats =>  {
+			this.listingStatistics = Object.assign(new RecruiterListingStatistics(), stats);
+			this.switchListing(this.selectedListing);
+			//this.switchTab(this.currentTab);
+		});
+		
 	}
 		
 	/**
@@ -99,13 +155,26 @@ export class RecruiterStatsComponent {
 	* Select the listing to display statistics for 
 	*/
 	public switchListing(listingId:string):void{
+		
 		this.selectedListing = listingId;
 	
 		let listingStat = this.listingStatistics.listingStats.filter(l => l.listingId == listingId)[0]
 		let stats 		= listingStat.stats;
 	
-		//this.listingViewsChart.chartData 	= [{ data: stats.map(s => s.count), label: listingStat.title },];
-		//this.listingViewsChart.chartLabels 	= stats.map(s => s.bucket.toString());
+		let labels:string[] = stats.map(s => ''+s.bucket);
+		
+		this.selectedListingsChartLabels = labels;
+		
+		let values = this.selectedListingsChartData = [
+			           	{
+			            label: "Listing View",
+			       	    data: stats.map(s => ''+s.count),
+			            backgroundColor: this.chartColor
+			          },
+			        	];
+		console.log("labels = " + JSON.stringify(labels));
+		
+		this.createTopChart(labels, values);
 		
 	}
 	
@@ -114,7 +183,105 @@ export class RecruiterStatsComponent {
 	* @param tabId - Id of Tab to display 
 	*/
 	public switchTab(tabId:string):void{
+		
 		this.currentTab = tabId;
+		
+		if(this.topChart){
+			this.topChart.destroy();
+		}
+		
+		if(this.bottomChart){
+			this.bottomChart.destroy();
+		}
+		
+		if(this.currentTab == 'general-stats'){
+			this.createTopChart(this.availabilityChartLabels, this.availabilityChartData);	
+			this.createBottomChart(this.listingsChartLabels, this.listingsChartData);
+			
+		}else {
+			this.selectedListing = '';
+			this.createTopChart(this.availabilityChartLabels, this.availabilityChartData);	
+			if(this.topChart){
+				this.topChart.destroy();
+			}
+		}
+		
 	}
+	
+	availabilityChartData = [
+           	{
+            label: "Sales",
+            data: ['467','576', '572', '79', '92',
+								 '574', '573', '576'],
+            backgroundColor: 'blue'
+          },
+        	];
+	availabilityChartLabels = ['2022-05-10', '2022-05-11', '2022-05-12','2022-05-13','2022-05-14', '2022-05-15', '2022-05-16','2022-05-17', ];
+	
+	listingsChartData = [
+	           	{
+	            label: "Sales",
+	            data: ['467','576', '572', '79', '92',
+									 '574', '573', '576'],
+	            backgroundColor: 'blue'
+	          },
+	        	];
+	listingsChartLabels = ['2022-05-10', '2022-05-11', '2022-05-12','2022-05-13','2022-05-14', '2022-05-15', '2022-05-16','2022-05-17', ];
+	
+	selectedListingsChartData = [
+	           	{
+	            label: "Sales",
+	            data: ['467','576', '572', '79', '92',
+									 '574', '573', '576'],
+	            backgroundColor: 'blue'
+	          },
+	        	];
+	selectedListingsChartLabels = ['2022-05-10', '2022-05-11', '2022-05-12','2022-05-13','2022-05-14', '2022-05-15', '2022-05-16','2022-05-17', ];
+	
+	
+	createTopChart(leftChartLabels:string[], topChartData:any){
+		
+  		if(this.topChart){
+  			this.topChart.destroy();
+  		}
+  		
+	    this.topChart = new Chart("topChart", {
+	      type: 'bar', //this denotes tha type of chart
+	
+	      data: {// values on X-Axis
+	        labels: leftChartLabels, 
+		       datasets: topChartData
+	      },
+	      options: {
+	        aspectRatio:2.5
+	      }
+	      
+	    });
+	   
+  	}
+	
+	public topChart: any;
+	public bottomChart: any;
+	
+	createBottomChart(chartLabels:string[], chartData:any){
+		
+  		if(this.bottomChart){
+  			this.bottomChart.destroy();
+  		}
+  		
+	    this.bottomChart = new Chart("bottomChart", {
+	      type: 'bar', //this denotes tha type of chart
+	
+	      data: {// values on X-Axis
+	        labels: chartLabels, 
+		       datasets: chartData
+	      },
+	      options: {
+	        aspectRatio:2.5
+	      }
+	      
+	    });
+	    
+  	}
 	
 }
