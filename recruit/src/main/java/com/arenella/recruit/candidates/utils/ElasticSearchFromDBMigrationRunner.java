923be2e394a28033d30aef576a15585ad846f982
kevin parkings <kparkings@gmail.com>
2024-05-05 02:15:08 +0200

    es properties

--- recruit/src/main/java/com/arenella/recruit/candidates/utils/ElasticSearchFromDBMigrationRunner.java
+++ recruit/src/main/java/com/arenella/recruit/candidates/utils/ElasticSearchFromDBMigrationRunner.java
@@ -1,31 +1,31 @@
 package com.arenella.recruit.candidates.utils;
 
 
-import java.util.stream.StreamSupport;
+//import java.util.stream.StreamSupport;
 
-import javax.annotation.PostConstruct;
+//import javax.annotation.PostConstruct;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
 
-import com.arenella.recruit.candidates.beans.Candidate;
-import com.arenella.recruit.candidates.dao.CandidateDao;
-import com.arenella.recruit.candidates.entities.CandidateDocument;
-import com.arenella.recruit.candidates.entities.CandidateEntity;
-import com.arenella.recruit.candidates.repos.CandidateRepository;
+//import com.arenella.recruit.candidates.beans.Candidate;
+//import com.arenella.recruit.candidates.dao.CandidateDao;
+//import com.arenella.recruit.candidates.entities.CandidateDocument;
+//import com.arenella.recruit.candidates.entities.CandidateEntity;
+//import com.arenella.recruit.candidates.repos.CandidateRepository;
 
-@Component
+//@Component
 public class ElasticSearchFromDBMigrationRunner {
 
 	
-	@Autowired
-	private CandidateDao 		candidateDao;
+	//@Autowired
+	//private CandidateDao 		candidateDao;
 	
-	@Autowired
-	private CandidateRepository candidateRepo;
+	//@Autowired
+	//private CandidateRepository candidateRepo;
 	
 	
-	@PostConstruct
+	//@PostConstruct
 	public void migrateDBtoElasticsearch() {
 		
 		//StreamSupport.stream(this.candidateDao.findAll().spliterator(),false).forEach(candidateEntity -> {
